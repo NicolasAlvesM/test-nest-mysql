6572f87ff0a981d5ec48bee65e23ea220de9d14e
NicolasAlvesM <nicolas.morenoam@outlook.com>
2024-09-20 16:59:07 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('API docs')
     .setDescription('Esta é uma documentação do projeto de Tarefas com NestJS')
